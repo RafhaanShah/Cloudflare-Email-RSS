a3aac217a9bc16cb918da7513b7266e7ef30fda4
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2025-10-05 18:21:16 +0100

    Add more unit test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,28 +2,16 @@ import { fetchMock, env, createExecutionContext, waitOnExecutionContext, SELF } 
 import { beforeEach, afterEach, describe, it, expect, vi } from 'vitest';
 import worker from '../src/index';
 import rawEmail from './fixtures/email.http?raw';
+import rawEmail2 from './fixtures/email2.http?raw';
 import rawFeed from './fixtures/feed.xml?raw';
+import rawFeed2 from './fixtures/feed2.xml?raw';
 import rawEntry from './fixtures/entry.xml?raw';
+import rawEntry2 from './fixtures/entry2.xml?raw';
 import { XMLParser } from 'fast-xml-parser';
 
 const fakeFeedFile = 'sender-domain-com.xml';
 const fakeEntryFile = 'domain.com/sender/message-id.html';
-const fakeEmail: ForwardableEmailMessage = {
-  raw: streamFromString(rawEmail),
-  headers: new Headers(),
-  rawSize: new TextEncoder().encode(rawEmail).length,
-  setReject: function (_reason: string): void {
-    throw new Error('Function not implemented.');
-  },
-  forward: function (_rcptTo: string, _headers?: Headers): Promise<void> {
-    throw new Error('Function not implemented.');
-  },
-  reply: function (_message: EmailMessage): Promise<void> {
-    throw new Error('Function not implemented.');
-  },
-  from: 'sender@example.com',
-  to: 'receiver@example.com',
-};
+const fakeEntryFile2 = 'domain.com/sender/message-id2.html';
 
 // create context for each test
 var ctx: ExecutionContext;
@@ -54,7 +42,7 @@ describe('Email-RSS Worker', () => {
   });
   it('uploads a new feed and entry', async () => {
     // wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-    await worker.email(fakeEmail, env, ctx);
+    await worker.email(createEmail(rawEmail), env, ctx);
     await waitOnExecutionContext(ctx);
 
     const addedFeed = await getBucketItemContent(fakeFeedFile);
@@ -62,6 +50,19 @@ describe('Email-RSS Worker', () => {
 
     const addedEntry = await getBucketItemContent(fakeEntryFile);
     expect(addedEntry).toEqual(rawEntry);
+  });
+
+  it('adds a new entry to an existing feed', async () => {
+    await env.RSS_BUCKET.put(fakeFeedFile, rawFeed);
+
+    await worker.email(createEmail(rawEmail2), env, ctx);
+    await waitOnExecutionContext(ctx);
+
+    const addedFeed = await getBucketItemContent(fakeFeedFile);
+    assertXmlObjects(addedFeed, rawFeed2);
+
+    const addedEntry = await getBucketItemContent(fakeEntryFile2);
+    expect(addedEntry).toEqual(rawEntry2);
   });
 });
 
@@ -89,4 +90,23 @@ function assertXmlObjects(actual: string | null | undefined, expected: string) {
   const act = parser.parse(actual);
   const exp = parser.parse(expected);
   expect(act).toEqual(exp);
+}
+
+function createEmail(body: string): ForwardableEmailMessage {
+  return {
+    raw: streamFromString(body),
+    headers: new Headers(),
+    rawSize: new TextEncoder().encode(body).length,
+    setReject: function (_reason: string): void {
+      throw new Error('Function not implemented.');
+    },
+    forward: function (_rcptTo: string, _headers?: Headers): Promise<void> {
+      throw new Error('Function not implemented.');
+    },
+    reply: function (_message: EmailMessage): Promise<void> {
+      throw new Error('Function not implemented.');
+    },
+    from: 'sender@example.com',
+    to: 'receiver@example.com',
+  };
 }
